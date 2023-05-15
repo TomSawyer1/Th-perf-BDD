aacf43027cf32323acd5c414559316b83e16271c
TomSawyer1 <117282498+TomSawyer1@users.noreply.github.com>
2023-05-15 12:51:49 +0200

    sql

--- thperf.sql
+++ thperf.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost:8889
--- Généré le : ven. 05 mai 2023 à 18:18
--- Version du serveur : 5.7.34
--- Version de PHP : 7.4.21
+-- Hôte : 127.0.0.1:3306
+-- Généré le : lun. 15 mai 2023 à 10:50
+-- Version du serveur : 8.0.31
+-- Version de PHP : 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,12 +27,16 @@ SET time_zone = "+00:00";
 -- Structure de la table `achat_car`
 --
 
-CREATE TABLE `achat_car` (
-  `id_achat` int(10) NOT NULL,
-  `user_id` int(10) NOT NULL,
-  `car_id` int(10) NOT NULL,
-  `nb_voiture` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `achat_car`;
+CREATE TABLE IF NOT EXISTS `achat_car` (
+  `id_achat` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `car_id` int NOT NULL,
+  `nb_voiture` int NOT NULL,
+  PRIMARY KEY (`id_achat`),
+  KEY `user_id` (`user_id`),
+  KEY `car_id` (`car_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
@@ -40,21 +44,38 @@ CREATE TABLE `achat_car` (
 -- Structure de la table `car`
 --
 
-CREATE TABLE `car` (
-  `id_car` int(10) NOT NULL,
+DROP TABLE IF EXISTS `car`;
+CREATE TABLE IF NOT EXISTS `car` (
+  `id_car` int NOT NULL AUTO_INCREMENT,
   `color` varchar(100) NOT NULL,
-  `nb_door` tinyint(4) NOT NULL,
-  `power` int(10) NOT NULL,
+  `nb_door` tinyint NOT NULL,
+  `power` int NOT NULL,
   `miles` varchar(50) NOT NULL,
-  `fuel` enum('Gazoil','Essence','Electric','Ethanol','Hybrid') NOT NULL,
+  `fuel` enum('gazoil','essence','electric','ethanol','hybrid') NOT NULL,
   `creation_year` date NOT NULL,
   `title` varchar(255) NOT NULL,
   `description` text NOT NULL,
-  `price` int(20) NOT NULL,
-  `id_cat` int(11) NOT NULL,
+  `price` int NOT NULL,
+  `id_cat` int NOT NULL,
   `picture` varchar(200) NOT NULL,
-  `transmission` enum('Manual','Automatique') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `transmission` enum('Manual','Automatique') NOT NULL,
+  PRIMARY KEY (`id_car`),
+  KEY `id_cat` (`id_cat`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;
+
+--
+-- Déchargement des données de la table `car`
+--
+
+INSERT INTO `car` (`id_car`, `color`, `nb_door`, `power`, `miles`, `fuel`, `creation_year`, `title`, `description`, `price`, `id_cat`, `picture`, `transmission`) VALUES
+(1, 'red', 4, 12, '12', 'essence', '0000-00-00', 'BMW', 'top sprots car', 123, 1, '', 'Manual'),
+(6, 'red', 4, 120, '120', 'essence', '0000-00-00', 'BMW', 'top sport car', 120, 1, '', 'Manual'),
+(7, 'red', 120, 4, '120', 'essence', '0000-00-00', 'BMW', 'top sport car', 120, 3, '', 'Manual'),
+(8, 'red', 120, 4, '120', 'essence', '0000-00-00', 'BMW', 'top sport car', 120, 5, '', 'Manual'),
+(9, 'blue', 4, 325, '556', 'essence', '0000-00-00', 'BMW', 'top sport car', 660, 2, '', 'Manual'),
+(10, 'red', 4, 12, '5000', 'gazoil', '0000-00-00', 'BMW', 'top sport car', 90000, 5, '', 'Manual'),
+(11, 'red', 4, 45, '5000', 'gazoil', '0000-00-00', 'BMW', 'top sport car', 90000, 5, '', 'Manual'),
+(12, 'blue', 4, 12, '7500', 'essence', '0000-00-00', 'BMW', 'Top sport car', 66000, 5, '', 'Automatique');
 
 -- --------------------------------------------------------
 
@@ -62,10 +83,12 @@ CREATE TABLE `car` (
 -- Structure de la table `categorie`
 --
 
-CREATE TABLE `categorie` (
-  `id_cat` int(11) NOT NULL,
-  `name` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `categorie`;
+CREATE TABLE IF NOT EXISTS `categorie` (
+  `id_cat` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_cat`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
 
 --
 -- Déchargement des données de la table `categorie`
@@ -85,83 +108,25 @@ INSERT INTO `categorie` (`id_cat`, `name`) VALUES
 -- Structure de la table `user`
 --
 
-CREATE TABLE `user` (
-  `id_user` int(100) NOT NULL,
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id_user` int NOT NULL AUTO_INCREMENT,
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
   `mail` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `address` text NOT NULL,
   `data_creation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `statut` tinyint(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `statut` tinyint NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3;
 
 --
 -- Déchargement des données de la table `user`
 --
 
 INSERT INTO `user` (`id_user`, `first_name`, `last_name`, `mail`, `password`, `address`, `data_creation`, `statut`) VALUES
-(9, 'Thomas', 'Sauce', 'Chateau@gmail.com', 'Thomas', '34 Avenue', '2023-04-17 17:15:45', 0),
-(10, 'Maison', 'Chateau ', 'Rouge@gmail.com', 'Rouge', '34 Avenue', '2023-04-27 13:22:14', 0);
-
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `achat_car`
---
-ALTER TABLE `achat_car`
-  ADD PRIMARY KEY (`id_achat`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `car_id` (`car_id`);
-
---
--- Index pour la table `car`
---
-ALTER TABLE `car`
-  ADD PRIMARY KEY (`id_car`),
-  ADD KEY `id_cat` (`id_cat`);
-
---
--- Index pour la table `categorie`
---
-ALTER TABLE `categorie`
-  ADD PRIMARY KEY (`id_cat`);
-
---
--- Index pour la table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id_user`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `achat_car`
---
-ALTER TABLE `achat_car`
-  MODIFY `id_achat` int(10) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `car`
---
-ALTER TABLE `car`
-  MODIFY `id_car` int(10) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `categorie`
---
-ALTER TABLE `categorie`
-  MODIFY `id_cat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT pour la table `user`
---
-ALTER TABLE `user`
-  MODIFY `id_user` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+(9, 'Thomas', 'Sauce', 'Chateau@gmail.com', 'Thomas', '34 Avenue', '2023-04-17 17:15:45', 0);
 
 --
 -- Contraintes pour les tables déchargées
@@ -171,8 +136,8 @@ ALTER TABLE `user`
 -- Contraintes pour la table `achat_car`
 --
 ALTER TABLE `achat_car`
-  ADD CONSTRAINT `achat_car_ibfk_1` FOREIGN KEY (`car_id`) REFERENCES `car` (`id_car`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `achat_car_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `achat_car_ibfk_1` FOREIGN KEY (`car_id`) REFERENCES `car` (`id_car`),
+  ADD CONSTRAINT `achat_car_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`);
 
 --
 -- Contraintes pour la table `car`
