15db76a8600354a16acb8762cefe37e94549ba9d
TomSawyer1 <117282498+TomSawyer1@users.noreply.github.com>
2023-05-17 14:54:23 +0200

    View voiture

--- views/car/voiture.php
+++ views/car/voiture.php
@@ -1,4 +1,22 @@
+<?php $allCars = car::showDb(); ?>
 <?php include VIEWS . 'inc/header.php'; ?>
+
+
+
+<?php
+for ($i = 0; $i < 9; $i++) {
+    if ($_GET['id'] == $allCars[$i]['id_car']) {
+?>
+        <h1 class="text-center my-5"><?= $allCars[$i]['title'] ?></h1>
+
+        <p class="text-center my-5"><?= $allCars[$i]['description'] ?></p>
+<?php
+    }
+}
+?>
+
+
+
 
 
 
