d30465859a74536e27ed5e513d539240fb2a7d5f
TomSawyer1 <117282498+TomSawyer1@users.noreply.github.com>
2023-06-09 00:30:03 +0200

    Updapte header.php
    j'ai rajouter le css

--- views/inc/header.php
+++ views/inc/header.php
@@ -21,7 +21,62 @@ if ($connected == 'false') {
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 	<link rel="stylesheet" href="<?= ROOT . "reset.css" ?>">
 	<link rel="stylesheet" href="<?= ROOT . "style.css" ?>">
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Fugaz+One&family=Rubik:wght@400;700&display=swap" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/a67ce56d07.js" crossorigin="anonymous"></script>
+	<script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+	<script src="main.js" defer></script>
+
 </head>
+
+<header>
+	<nav id="nav1" class="wrapper">
+		<div class="logo">
+			<a href="index.html"><img src="Logo/logo3pm1.png" id="logo" alt="logo"></a>
+		</div>
+		<div class="menu">
+			<ul>
+				<li><a href="#">History</a></li>
+				<li class="deroulant">
+					<a href="#">
+						History
+					</a>
+					<ul class="sub-menu">
+						<li><a href="#">Tous les modèles </a></li>
+						<li><a href="#">M3 E30</a></li>
+						<li><a href="#">M3 E36</a></li>
+						<li><a href="#">M3 E46</a></li>
+						<li><a href="#">M3 E90</a></li>
+						<li><a href="#">M3 F80</a></li>
+					</ul>
+				</li>
+				<li><a href="#">History</a></li>
+				<li><a href="#">History</a></li>
+			</ul>
+		</div>
+		<div class="outil">
+			<ul>
+				<li><i class="fa-solid fa-magnifying-glass"></i></li>
+				<li><i class="fa-solid fa-cart-shopping"></i></li>
+				<li class="deroulant">
+					<a href="#">
+						<i class="fa-regular fa-user">
+						</i>
+					</a>
+					<ul class="sub-menu">
+						<li><a href="#">Connexion</a></li>
+						<li><a href="#">Insciption</a></li>
+						<li><a href="#">Profil</a></li>
+						<li><a href="#">Deconnexion</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</nav>
+
+</header>
 
 <body>
 	<nav class="navbar navbar-expand-lg navbar-dark p-3 " style="background-color:#1C2331;" id="headerNav">
