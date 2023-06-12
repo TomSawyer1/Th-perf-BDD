0080f277f7f47ddfcbd0406aa0176b195f424e69
TomSawyer1 <117282498+TomSawyer1@users.noreply.github.com>
2023-06-12 13:08:45 +0200

    css and php

--- views/user/connexion.php
+++ views/user/connexion.php
@@ -4,9 +4,35 @@ include VIEWS . 'inc/header.php';
 
 ?>
 
-<form method="post" action="">
+<div class="container">
+  <div class="img">
+    <img src="<?= ROOT . "IMGM3/3f90cs.jpeg" ?>" alt="3F90">
+  </div>
+  <div class="login-box">
+    <h2>Connexion</h2>
 
-  <section class="vh-100" style="background-color: #1c2e31;">
+    <form method="post" action="">
+      <?= isset($_SESSION["message"]) ? $_SESSION["message"] : "";
+
+      $_SESSION["message"] = "";
+      ?>
+      <div class="user-box">
+        <!-- <input type="text" name="username" required> -->
+        <!-- <label>Nom d'utilisateur</label> -->
+
+        <label class="form-label" for="typeEmailX-2">Email</label>
+        <input type="email" id="typeEmailX-2" class="form-control form-control-lg" name="mail" value="<?= !empty($_SESSION['mail']) ? $_SESSION['mail'] : ""; ?>">
+      </div>
+      <div class="user-box">
+        <label class="form-label" for="typePasswordX-2">Mot de passe</label>
+        <input type="password" id="typePasswordX-2" class="form-control form-control-lg" name="password" />
+      </div>
+      <button type="submit">Se connecter</button>
+      <p class="message">Pas encore inscrit ? <a href="#">S'inscrire</a></p>
+    </form>
+  </div>
+
+  <!-- <section class="vh-100" style="background-color: #1c2e31;">
     <div class="container py-5 h-100">
       <div class="row d-flex justify-content-center align-items-center h-100">
         <div class="col-12 col-md-8 col-lg-6 col-xl-5">
@@ -36,18 +62,14 @@ include VIEWS . 'inc/header.php';
           </div>
         </div>
       </div>
-  </section>
+  </section> -->
+
+  </form>
 
 
+  <?php
 
-
-
-</form>
-
-
-<?php
-
-var_dump($_SESSION['user']);
-//UserController::connexion();
-include VIEWS . 'inc/footer.php';
-?>
+  // var_dump($_SESSION['user']);
+  //UserController::connexion();
+  include VIEWS . 'inc/footer.php';
+  ?>
