f0228dc470bdd4d4b6e5e2c1fcfaec2a3bc3a1f4
TomSawyer1 <117282498+TomSawyer1@users.noreply.github.com>
2023-06-27 21:14:39 +0200

    Fav icon

--- views/inc/header.php
+++ views/inc/header.php
@@ -21,7 +21,7 @@ if ($connected == 'false') {
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link rel="shortcut icon" href="<?= ASSET; ?>Logo/logo3pm1.png" type="image/x-icon">
+	<link rel="shortcut icon" href="<?= ASSET; ?>Logo/Calq.png" type="image/x-icon">
 	<link href="https://fonts.googleapis.com/css2?family=Fugaz+One&family=Rubik:wght@400;700&display=swap" rel="stylesheet">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 	<script src="https://kit.fontawesome.com/a67ce56d07.js" crossorigin="anonymous"></script>
